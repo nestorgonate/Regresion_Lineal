574627e953c202b99d0ceb69f187cc053e121ba8
nestorgonate <negavilaneson@uide.edu.ec>
2026-03-28 11:53:46 -0500

    ADD: Denormalizacion de datos, prediccion de temperatura en base a un input

--- calculos/regresion.go
+++ calculos/regresion.go
@@ -1,9 +1,78 @@
 package calculos
 
-func RegresionLineal(datos, pesos []float64, sesgo float64) float64{
+import (
+	"fmt"
+	"regresionlineal/models"
+)
+
+func RegresionLineal(datos, pesos []float64, sesgo float64) float64 {
 	var prediccion float64
-	for i:=0;i<len(datos);i++{
-		prediccion = pesos[i]*datos[i]
+	for i := 0; i < len(datos); i++ {
+		prediccion = pesos[i] * datos[i]
 	}
-	return prediccion+sesgo
+	return prediccion + sesgo
+}
+
+func Entrenamiento(iteraciones, numFeatures, muestras int, targetY []float64,
+					featuresX [][]float64) models.Aprendizaje{
+	aprendizaje := models.Aprendizaje{
+		Pesos:           []float64{1, 0.1, 0.1},
+		Sesgo:           0.0,
+		TasaAprendizaje: 0.01,
+	}
+	//Entrenamiento
+	for i := 0; i < iteraciones; i++ {
+		var errorAcumuladoSesgo float64 //Sumatoria de errores de prediccion
+		errorAcumuladoPesos := make([]float64, numFeatures)
+		var errorCuadratico float64
+		//Itera j que es cada tiempo del clima
+		for j := 0; j < muestras; j++ {
+			//Regesion lineal simple: w*x+b
+			prediccion := RegresionLineal(featuresX[j], aprendizaje.Pesos, aprendizaje.Sesgo)
+			//Obtener error de prediccion
+			errorPrediccion := prediccion - targetY[j]
+			//Elevar el error de prediccion al cuadrado
+			errorCuadratico += errorPrediccion * errorPrediccion
+			//Sumatoria del error en la prediccion (Sumatoria del error)
+			errorAcumuladoSesgo += errorPrediccion
+			//Itera k que es cada valor de j para actualizar el peso
+			for k := 0; k < numFeatures; k++ {
+				//Calcular gradiente del peso (Sumatoria del error * x)
+				errorAcumuladoPesos[k] += errorPrediccion * featuresX[j][k]
+			}
+		}
+		//Actualizer parametros
+		n := float64(muestras)
+		//Actualizar sesgo sesgo=sesgo-tasaAprendizaje*(Σerror/n)
+		aprendizaje.Sesgo -= aprendizaje.TasaAprendizaje * (errorAcumuladoSesgo / n)
+		//Actualizar cada peso peso=peso-tasaAprendizaje(Σ(error*x)/n)
+		for k := 0; k < numFeatures; k++ {
+			aprendizaje.Pesos[k] -= aprendizaje.TasaAprendizaje * (errorAcumuladoPesos[k] / n)
+		}
+		//Calcular error cuadratico medio
+		errorCuadraticoMedio := errorCuadratico / n
+		if i%10 == 0 {
+			fmt.Printf("Iteración %d - Sesgo: %v, Pesos: %v\n, Error: %v\n", i, aprendizaje.Sesgo, aprendizaje.Pesos, errorCuadraticoMedio)
+		}
+	}
+	return aprendizaje
+}
+
+//Desnormalizar un valor usando x=(x_normalizado*(x_max-x_min))+x_min
+func Desnormalizar(valorNormalizado, x_min, x_max float64) float64 {
+    return (valorNormalizado * (x_max - x_min)) + x_min
+}
+
+func PredecirTemperatura(relative_humidity, precipitation, pressure float64,
+						aprendizaje models.Aprendizaje, x_min_max map[string]map[string]float64) float64{
+	normalizarHumedad := MinMaxScaler(relative_humidity, x_min_max["relative_humidity"]["x_min"], x_min_max["relative_humidity"]["x_max"])
+	normalizarPrecipitacion := MinMaxScaler(precipitation, x_min_max["precipitation"]["x_min"], x_min_max["precipitation"]["x_max"])
+	normalizarPresion := MinMaxScaler(pressure, x_min_max["pressure"]["x_min"], x_min_max["pressure"]["x_max"])
+	//Prediccion y = w1*x1+w2*x2+w3*x3+b
+	temperatura := (aprendizaje.Pesos[0]*normalizarHumedad)+
+					(aprendizaje.Pesos[1]*normalizarPrecipitacion)+
+					(aprendizaje.Pesos[2]*normalizarPresion)+
+					aprendizaje.Sesgo
+	temperaturaCelsius := Desnormalizar(temperatura, x_min_max["temperature"]["x_min"], x_min_max["temperature"]["x_max"])
+	return temperaturaCelsius
 }
--- main.go
+++ main.go
@@ -4,13 +4,12 @@ import (
 	"fmt"
 	"regresionlineal/calculos"
 	"regresionlineal/data"
-	"regresionlineal/models"
 )
 
 func main() {
 	//Programa para predecir la temperatura en base a relative_humidity,precipitation,pressure
 	csvData := data.ReadCsv()
-	for key, value := range csvData{
+	for key, value := range csvData {
 		fmt.Printf("Cantidad de datos de %v: %v\n", key, len(value))
 	}
 	fmt.Printf("%v\n", "-----------")
@@ -47,14 +46,18 @@ func main() {
 	fmt.Printf("%v\n", "-----------")
 	//Debido a que no todos los datos siguen una distribucion normal se normaliza por min max scaler
 	normalizeData := make(map[string][]float64)
+	x_min_max := make(map[string]map[string]float64)
 	for key, slice := range csvData {
 		x_min, x_max := slice[0], slice[0]
+		x_min_max[key] = make(map[string]float64)
 		for _, value := range slice {
 			if value < x_min {
 				x_min = value
+				x_min_max[key]["x_min"] = x_min
 			}
 			if value > x_max {
 				x_max = value
+				x_min_max[key]["x_max"] = x_max
 			}
 		}
 		//Itera segun el numero de filas en cada columna
@@ -62,6 +65,9 @@ func main() {
 			minMax := calculos.MinMaxScaler(slice[i], x_min, x_max)
 			normalizeData[key] = append(normalizeData[key], minMax)
 		}
+	}
+	for key, value := range x_min_max {
+		fmt.Printf("%v: X_min:%v X_max: %v\n", key, value["x_min"], value["x_max"])
 	}
 	fmt.Printf("%v\n", "-----------")
 	//Configuraciones iniciales
@@ -80,45 +86,19 @@ func main() {
 	fmt.Printf("%v\n", "-----------")
 	numFeatures := len(featuresX[0])
 	iteraciones := 1000
-	aprendizaje := models.Aprendizaje{
-		Pesos:           []float64{0.1, 0.1, 0.1},
-		Sesgo:           0.0,
-		TasaAprendizaje: 0.01,
-	}
-	//Entrenamiento
-	for i := 0; i < iteraciones; i++ {
-		var errorAcumuladoSesgo float64 //Sumatoria de errores de prediccion
-		errorAcumuladoPesos := make([]float64, numFeatures)
-		var errorCuadratico float64
-		//Itera j que es cada tiempo del clima
-		for j := 0; j < muestras; j++ {
-			//Regesion lineal simple: w*x+b
-			prediccion := calculos.RegresionLineal(featuresX[j], aprendizaje.Pesos, aprendizaje.Sesgo)
-			//Obtener error de prediccion
-			errorPrediccion := prediccion - targetY[j]
-			//Elevar el error de prediccion al cuadrado
-			errorCuadratico += errorPrediccion * errorPrediccion
-			//Sumatoria del error en la prediccion (Sumatoria del error)
-			errorAcumuladoSesgo += errorPrediccion
-			//Itera k que es cada valor de j para actualizar el peso
-			for k := 0; k < numFeatures; k++ {
-				//Calcular gradiente del peso (Sumatoria del error * x)
-				errorAcumuladoPesos[k] += errorPrediccion * featuresX[j][k]
-			}
-		}
-		//Actualizer parametros
-		n := float64(muestras)
-		//Actualizar sesgo sesgo=sesgo-tasaAprendizaje*(Σerror/n)
-		aprendizaje.Sesgo -= aprendizaje.TasaAprendizaje * (errorAcumuladoSesgo / n)
-		//Actualizar cada peso peso=peso-tasaAprendizaje(Σ(error*x)/n)
-		for k := 0; k < numFeatures; k++ {
-			aprendizaje.Pesos[k] -= aprendizaje.TasaAprendizaje * (errorAcumuladoPesos[k] / n)
-		}
-		//Calcular error cuadratico medio
-		errorCuadraticoMedio := errorCuadratico/n
-		if i%10 == 0 {
-			fmt.Printf("Iteración %d - Sesgo: %.5f, Pesos: %.5f\n, Error: %.5f\n", i, aprendizaje.Sesgo, aprendizaje.Pesos, errorCuadraticoMedio)
-		}
-	}
+	aprendizaje := calculos.Entrenamiento(iteraciones, numFeatures, muestras, targetY, featuresX)
+	fmt.Printf("%v\n", "-----------")
+	fmt.Printf("%v\n", aprendizaje)
+	fmt.Printf("%v\n", "-----------")
+	var relative_humidity, precipitation, pressure float64
+	fmt.Print("Ingresa la humedad relativa: \n")
+	fmt.Scan(&relative_humidity)
+	fmt.Print("Ingresa la precipitacion: \n")
+	fmt.Scan(&precipitation)
+	fmt.Print("Ingresa la presion a nivel del mar: \n")
+	fmt.Scan(&pressure)
+	temperatura := calculos.PredecirTemperatura(relative_humidity, precipitation, pressure, aprendizaje, x_min_max)
+	fmt.Printf("%v\n", "-----------")
+	fmt.Printf("Temperatura predicha por la regresion lineal: %v\n", temperatura)
 	fmt.Printf("%v\n", "-----------")
 }
